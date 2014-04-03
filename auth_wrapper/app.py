7368d2bc09296ad2fe51320fe1752d44ae25b852
Michael Groening <michael.groening@intosite.de>
2014-04-03 18:00:09 +0200

    HAAANDS

--- auth_wrapper/app.py
+++ auth_wrapper/app.py
@@ -13,32 +13,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
 class Client(db.Model):
     __tablename__ = 'mormont_clients'
-    id = db.Column(db.Integer, primary_key=True)
+    pk = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.String, nullable=False)
     secret = db.Column(db.String, nullable=False)
 
 
 class AccessToken(db.Model):
     __tablename__ = 'mormont_accesstokens'
-    id = db.Column(db.Integer, primary_key=True)
+    pk = db.Column(db.Integer, primary_key=True)
     access_token = db.Column(db.String, nullable=False)
     client = db.Column(
         db.Integer,
-        db.ForeignKey('mormont_clients.id'),
+        db.ForeignKey('mormont_clients.pk'),
         nullable=False,
     )
     salt = db.Column(db.String, nullable=False)
@@ -50,37 +38,80 @@ class AccessToken(db.Model):
 def login():
 
     def check_permission(client, hash):
-        own_hash = hmac.new(str(client.secret), str(client.id), sha1).hexdigest()
+        '''
+        check if the hash, a client sent us is valid
+        '''
+        own_hash = hmac.new(
+            str(client.secret),
+            str(client.pk),
+            sha1
+        )
+        own_hash.update(app.secret_key)
+        if app.debug:
+            print(own_hash.hexdigest())
+        if hash == own_hash.hexdigest():
+            return True
+
+    def create_token():
+        '''
+        create a new token and deactivate all other tokens
+        '''
+        client_id = request.form["client"]
+        print client_id
+        hash = request.form["hash"]
+        print hash
+        client = db.session.query(Client) \
+            .filter(Client.client_id == client_id) \
+            .first()
+
+        if client and check_permission(client, hash):
+            access_tokens = db.session.query(AccessToken).filter(AccessToken.client == client.pk).filter(AccessToken.active is True)
+
+            for token in access_tokens:
+                token.active = False
+                db.session.add(token)
+            db.session.commit()
+            token = generate_token(client, hash)
+            if token:
+                auth_token = {
+                    'token': token
+                }
+                return auth_token
+            else:
+                return None
+        else:
+            return None
+
+    def generate_token(client, hash):
+        '''
+        generate an access-token for a client
+        '''
+        own_hash = hmac.new(str(client.secret),
+                            str(client.pk),
+                            sha1
+        ).hexdigest()
         print(own_hash)
         if hash == own_hash:
             return True
 
-    def create_token():
-        client_id = request.form["client"]
-        hash = request.form["hash"]
-
-        client = db.session.query(Client).filter(Client.client_id == client_id).first()
-        if client and check_permission(client, hash):
-            auth_token = {
-                'token': generate_token(client)
-            }
-        return auth_token
-
-    def generate_token(client):
-
         rand = random.Random()
         token = AccessToken()
 
-        token.client = client.id
-        token.salt = rand.randint(0, 2**48)
+        token.client = client.pk
+        token.salt = rand.randint(0, 2 ** 48)
         token.creation_date = datetime.now()
 
-        hash = hmac.new(str(client.secret), str('client={}'.format(client.client_id)), sha1)
+        hash = hmac.new(
+            str(client.secret),
+            str('client={}'.format(client.client_id)),
+            sha1,
+        )
         hash.update(str('salt={}'.format(token.salt)))
         hash.update(str('timestamp={}'.format(token.creation_date.isoformat())))
         token.access_token = hash.hexdigest()
         token.active = True
-        print(token.access_token)
+        if app.debug:
+            print(token.access_token)
 
         db.session.add(token)
         db.session.commit()
@@ -88,13 +119,16 @@ def login():
 
     if request.method == 'POST':
         token = create_token()
-        return make_response(jsonify(token))
+        if token:
+            return make_response(jsonify(token))
+        else:
+            return make_response('access denied', 403)
 
     if request.method == 'GET':
         return render_template('login.html')
 
-def get_authenticated_user(session, request):
 
+def get_authenticated_user():
     try:
         if 'X-Access-Token' in request.headers:
             #import ipdb; ipdb.set_trace()
@@ -117,11 +151,11 @@ def authenticated(method):
     """Decorate methods with this to require that the user be logged in.
 
     If the user is not logged in, they will be redirected to the configured
-    `login url <RequestHandler.get_login_url>`.
+    `login`-URL.
     """
     @functools.wraps(method)
     def wrapper(*args, **kwargs):
-        client = get_authenticated_user(session , request)
+        client = get_authenticated_user()
         if not client:
             return redirect(url_for('.login'))
         else:
@@ -129,13 +163,23 @@ def authenticated(method):
         return method(*args, **kwargs)
     return wrapper
 
+
 @app.route('/logout')
 @authenticated
 def logout():
     client = g.get('client', None)
     if client:
-        db.session.query('AccessToken').filter(client == client.id)
-        import ipdb; ipdb.set_trace()
+        access_tokens = db.session.query(AccessToken).filter(
+            AccessToken.client == client.pk
+        ).filter(
+            AccessToken.active is True
+        )
+        for token in access_tokens:
+            token.active = False
+            db.session.add(token)
+        db.session.commit()
+    return make_response('OK', 200)
+
 
 @app.route('/')
 @authenticated
@@ -143,7 +187,21 @@ def hello_world():
     client = g.get('client')
     return 'Hello, {}!'.format(client.client_id)
 
+
 if __name__ == '__main__':
+
+    db.create_all()
+
+    if not db.session.query(Client).all():
+        print ("create dummy client")
+        dummy = Client()
+        dummy.client_id = 'foo'
+        print "foo"
+        dummy.secret = 'foo'
+        db.session.add(dummy)
+        db.session.commit()
+
+
     app.debug = True
     app.secret_key = "fooo"
     app.run()
